13aeb877f277071c085eaf6c28c031458d91711a
k-mccalla <kmccalla@qa.com>
2021-01-21 12:55:39 +0000

    Conditionals

---
+++ conditionals.java
@@ -0,0 +1,25 @@
+package com;
+
+public class conditionals {
+
+
+	   
+
+	    public static void  main(String[] args) {
+	        conditional();
+	    } 
+	    
+
+	    public static void conditional() {
+	    	int num1 = 500;
+	    			int num2 = 100; 
+	    			int result = num1 + num2;
+	        if (num1 >= num2) {
+	            System.out.println("No division");
+	        } else { 
+	        	System.out.println(result);
+	        }
+	        
+	  
+	    }
+	    }
--- operators.java
+++ operators.java
@@ -1,38 +1,44 @@
 package com;
 
 public class operators {
-	
 
-		public static void method1() {
-			int num1 = 150;
-		
-			int num2 = 5;
-	int result = num1 * num2;
-			
-			System.out.println(result);
-		}
+	public static void method1() {
+		int num1 = 150;
 
-		public static void method2() {
-			int num1 = 100;
-					int num2 = 50;
-					int result = num1 - num2;
-					
-			System.out.println(result);
-		
-		}
-		public static void method3() {
-			int num1 = 50;
-					int num2 = 5;
-					int result = num1 / num2;
-			System.out.println(result);
-		}
-		
-		
-		public static void main(String[] args) {
-			method1();
-			method2();
-			method3();
+		int num2 = 5;
+		int result = num1 * num2;
 
-		}
-		
+		System.out.println(result);
 	}
+
+	public static void method2() {
+		int num1 = 100;
+		int num2 = 50;
+		int result = num1 - num2;
+
+		System.out.println(result);
+
+	}
+
+	public static void method3() {
+		int num1 = 5;
+		int num2 = 50;
+		int result = num1 + num2;
+		if (num1 >= num2) {
+			System.out.println("Division cannot be performed");
+		} else {
+			System.out.println(result);
+		}
+	}
+	
+	
+        
+
+	public static void main(String[] args) {
+		method1();
+		method2();
+		method3();
+
+	}
+
+}
